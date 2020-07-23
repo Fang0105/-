37cfdaa14f0db67f472143acba1da3f2bc3579e7
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-07-23 23:54:39 +0800

    Change testcase

--- gen3.py
+++ gen3.py
@@ -10,3 +10,4 @@ for task in range(taskStart,taskStart+taskNum):
     with open(f"testcase/{task}.in","w") as f:
         for i in range(random.randrange(1,TCmaxN+1)):
             f.write(str(random.randrange(1,maxN+1))+"\n")
+        f.write("0\n")
